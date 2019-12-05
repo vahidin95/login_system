eddec9f0f8171f6afbb266fc497a9ca9401d7709
vahidin95 <vahidin.talic77@gmail.com>
2019-12-05 20:23:45 +0100

    fixing bugs in admin section

--- admin/admin_contect.php
+++ admin/admin_contect.php
@@ -1,51 +1,55 @@
 <?php
-include_once('dbh.inc.php');
+include_once('includes/dbh.inc.php');
 $now = time();
 if ($now > $_SESSION['expired']) {
     session_unset();
     session_destroy();
     header("Location: ../index.php?login=session_expired");
 }
-$uid = $_SESSION['u_id']
-$sql = "SELECT * FROM users WHERE NOT user_uid="vaha123";";
+$uid = $_SESSION['u_id'];
+$sql = "SELECT * FROM users WHERE NOT user_uid='vaha123';";
 $result = mysqli_query($conn, $sql);
-?>
 
-<!-- Admin view  -->
-<div class="row">
-  <?php
+
+
+
+
+//die(var_dump($user_photo));
+echo "<div class='row'>";
+
   $users = mysqli_num_rows($result);
   if($users > 0){
-    while ($row = mysqli_fetch_assoc($resultImg)) {
-      echo "<div>";
-      if ($row['user_status'] == 0) {
-        echo "
+    //nk
+    while($row = mysqli_fetch_assoc($result)){
+      //$uid = $row['user_id'];
+      $uid = $row['user_id'];
+      $user_photo = 'uploads/profile'.$uid.'.jpg';
+      ?>
+
         <div class='col-sm-6 col-md-4'>
           <div class='thumbnail'>
-            <img src='../uploads/profile$uid.jpg' class='thumbnail image'/>
+            <!--check if user have uploaded picture in direction -->
+
+            <?php if(file_exists($user_photo)){
+
+            echo "<img src='../uploads/profile$uid.jpg' class='thumbnail image'/>";
+
+          }else{
+             echo "<img src='../uploads/profiledefault.jpg' class='thumbnail image' />";
+             $status = $row['user_status'];
+             if ($status == 0) {
+               $status = "UPDATE users SET user_status=1 WHERE user_id='$uid'; ";
+               mysqli_query($conn, $status);
+             }
+        } ?>
+
             <div class='caption'>
-              <h3>Thumbnail label</h3>
+              <h3>User: <?= $row['user_first'];?></h3>
               <p>...</p>
               </div>
           </div>
-        </div>
-        ";
-      }else{
-        echo "
-        <div class='col-sm-6 col-md-4'>
-          <div class='thumbnail'>
-            <img src='../uploads/profiledefault.jpg' class='thumbnail image' />
-            <div class='caption'>
-              <h3>Thumbnail label</h3>
-              <p>...</p>
-              </div>
-          </div>
-        </div>
-        ";
-        $setstatus = "UPDATE users SET user_status=1 WHERE user_id='$uid'";
-        return $setstatus;
-      }
-      echo "</div>";
-    }
-  }?>
-</div>
+        </div> <?php
+      }// end while loop
+  }else {
+    echo "There is no more users except you Vahidine</div>";
+  }
